32fdba4b7bf3654b60606d6ec74efe63761bc3bf
lundow <eriklundow@gmail.com>
2020-05-04 14:27:28 +0200

    Updates

---
+++ precache-manifest.a8ff11897adfe9f68426cf6a9b373949.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db209d8b820309edfccaf9c2d47ca6e",
+    "url": "/lundow_website/index.html"
+  },
+  {
+    "revision": "908a280861502a526196",
+    "url": "/lundow_website/static/css/main.d0845f27.chunk.css"
+  },
+  {
+    "revision": "e50cad04511d2dca6afb",
+    "url": "/lundow_website/static/js/2.afd08c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lundow_website/static/js/2.afd08c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908a280861502a526196",
+    "url": "/lundow_website/static/js/main.dfba64fd.chunk.js"
+  },
+  {
+    "revision": "cf3c3a6716a1fe329e9f",
+    "url": "/lundow_website/static/js/runtime-main.d0d5b801.js"
+  },
+  {
+    "revision": "5abd8e60dcdbca042c876e53c9d17d38",
+    "url": "/lundow_website/static/media/CV_2020.5abd8e60.png"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/lundow_website/static/media/caret-down.ca82bc74.svg"
+  },
+  {
+    "revision": "5001607c73306858bf1fc1f6316ecc2c",
+    "url": "/lundow_website/static/media/github.5001607c.svg"
+  },
+  {
+    "revision": "9672db302541cbadca3840068bc7b378",
+    "url": "/lundow_website/static/media/instagram.9672db30.svg"
+  },
+  {
+    "revision": "1a5ae916da7a91eb1a9b2bd0012dd151",
+    "url": "/lundow_website/static/media/linkedIn.1a5ae916.svg"
+  },
+  {
+    "revision": "827c6984bf4dcfeae7aad4590a50dcf5",
+    "url": "/lundow_website/static/media/react.827c6984.svg"
+  }
+]);
